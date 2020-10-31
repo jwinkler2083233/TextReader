f2a1c9180111ae3997704676831b8194a6b336a4
jwinkler2083233 <jwinkler2083233@users.noreply.github.com>
2020-10-31 11:01:17 -0400

    Add files via upload
    Initial commit

---
+++ TextReader/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace TextReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Speak(textBox1.Text);
+        }
+    }
+}
